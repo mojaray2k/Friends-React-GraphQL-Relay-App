72a31792d3ff5a49162b26fc1e8589d02ba0ba3d
mojaray2k <amen.ra@imaginationeverywhere.info>
2018-02-01 04:24:28 -0500

    first inital complete project

---
+++ README.md
@@ -0,0 +1,2 @@
+# React/Relay/GraphQL Friends App
+
--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import path from 'path';
+import webpack from 'webpack';
+import WebPackDevServer from 'webpack-dev-server';
+import { schema } from './data/schema'; 
 
-const app = express();
+const APP_PORT = 3000;
+const GRAPHQL_PORT = 8080;
 
-app.get('/', (req, res) => {
-  res.send('GraphQL & Relay modern is cool!!!');
+// GraphQL server
+const graphQLServer = express();
+graphQLServer.use('/', graphqlHTTP({
+    schema: schema,
+    pretty: true,
+    graphiql: true,
+}));
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(`GraphQL server on localhost:${GRAPHQL_PORT}`));
+
+// Relay
+const compiler = webpack({
+    entry:['whatwg-fetch', path.resolve(__dirname, 'src', 'App.js')],
+    module: {
+        loaders: [
+            {
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                test: /\.js$/,
+            },
+        ],
+    },
+    output: {filename: 'App.js', path: '/'}
 });
 
-app.listen(8080, () => console.log('Running server on localhost:8080/graphql'));
+const app = new WebPackDevServer(compiler, {
+    contentBase: '/public/',
+    proxy: {'/graphql': `http://localhost:${GRAPHQL_PORT}`},
+    publicPath: '/src/',
+    stats: {colors: true},
+});
+
+app.use('/', express.static(path.resolve(__dirname, 'public')));
+app.listen(APP_PORT, () => console.log(`App is now running on localhost:${APP_PORT}`));
